c16cfd93dc019bbfc1492ca9d4d431981835d752
sudheer <sudheergupta0508@gmail.com>
2023-06-24 20:02:36 +0530

    theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_finance_app/pages/login.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'theams/colors.dart' as AppColors;
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
+        theme: ThemeData(
+          inputDecorationTheme: const InputDecorationTheme().copyWith(
+              alignLabelWithHint: true,
+              iconColor: AppColors.black,
+              hintStyle: const TextStyle(color: AppColors.black),
+              border: InputBorder.none),
+          primaryColor: AppColors.secondary,
+          fontFamily: GoogleFonts.poppins().fontFamily,
+          scaffoldBackgroundColor: AppColors.secondary,
+        ),
         home: const LoginPage());
   }
 }
