294ad29119f52816eaa70e4d39e67b8fe602d284
sudheer <sudheergupta0508@gmail.com>
2023-06-25 23:47:35 +0530

    bottom navbar add, incomplete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,14 +10,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _bottomNavIndex = 0;
+  List<IconData> iconList = [
+    CupertinoIcons.home,
+    CupertinoIcons.creditcard,
+    CupertinoIcons.money_dollar,
+    CupertinoIcons.person,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: getBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        //params
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.verySmoothEdge,
+        leftCornerRadius: 32,
+        rightCornerRadius: 32,
+        onTap: (index) => setState(() => _bottomNavIndex = index),
+        //other params
+      ),
     );
   }
 
   Widget getBody() {
-    return Text('Home page');
+    return SafeArea(child: Text('Home page'));
   }
 }
