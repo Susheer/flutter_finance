c744fe43b1301803934136013bc96faf7856c182
sudheer <sudheergupta0508@gmail.com>
2023-06-25 23:33:50 +0530

    code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_finance_app/pages/login.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'theams/colors.dart' as AppColors;
+import 'package:flutter_finance_app/theme/colors.dart' as AppColors;
 
 void main() {
   runApp(const MyApp());
@@ -13,17 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          inputDecorationTheme: const InputDecorationTheme().copyWith(
-              alignLabelWithHint: true,
-              iconColor: AppColors.black,
-              hintStyle: const TextStyle(color: AppColors.black),
-              border: InputBorder.none),
-          primaryColor: AppColors.secondary,
-          fontFamily: GoogleFonts.poppins().fontFamily,
-          scaffoldBackgroundColor: AppColors.secondary,
-        ),
-        home: const LoginPage());
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme().copyWith(
+            alignLabelWithHint: true,
+            iconColor: AppColors.black,
+            hintStyle: const TextStyle(color: AppColors.black),
+            border: InputBorder.none),
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        scaffoldBackgroundColor: AppColors.secondary,
+        primaryColor: AppColors.primary,
+      ),
+      home: const LoginPage(),
+    );
   }
 }
